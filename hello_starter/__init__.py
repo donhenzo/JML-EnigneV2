22c7fadbbae30d34b78c2e16f7384165d6cf3a08
donhenzo <129789268+donhenzo@users.noreply.github.com>
2026-08-21 11:16:33 +0100

    testing issue

--- hello_starter/__init__.py
+++ hello_starter/__init__.py
@@ -8,6 +8,6 @@ logger = logging.getLogger(__name__)
 
 async def main(req: func.HttpRequest, starter: str) -> func.HttpResponse:
     client = df.DurableOrchestrationClient(starter)
-    instance_id = await client.start_new("hello_orchestrator")
+    instance_id = await client.start_new("hello_orchestrator", None, None)
     logger.info("Started orchestration — instance_id=%s", instance_id)
     return client.create_check_status_response(req, instance_id)
